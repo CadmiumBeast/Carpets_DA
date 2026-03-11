1c411c6d434d15bc6a730debf88c04627abba840
cadmiumbeast <shakeelshajahan212@gmail.com>
2026-03-11 12:50:00 +0530

    URL

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -25,14 +24,6 @@ app.use('/api/stock', productStockRoutes);
 app.use('/api/quotations', quotationRoutes);
 app.use('/api/sitevisits', siteVisitRoutes);
 app.use('/api/customers', customerRoutes);
-
-// Serve static files from the client build
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// SPA catch-all: serve index.html for any non-API route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
